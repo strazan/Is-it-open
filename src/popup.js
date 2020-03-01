30ed3f4c2a77073e3660fd16c189dd6fb4aea199
strazan <sigge@labor.cool>
2020-03-01 11:06:57 +0100

    fix: improved UI

--- src/popup.js
+++ src/popup.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
-const input = document.getElementById('input');
+const input = document.getElementById("input");
 const autocomplete = new google.maps.places.Autocomplete(input);
-const outputOpenHours = document.getElementById('output-open-hours');
-const outputIsOpen = document.getElementById('output-isOpen');
-autocomplete.setTypes(['establishment']);
+const outputOpenHours = document.getElementById("output-open-hours");
+const outputIsOpen = document.getElementById("output-isOpen");
+autocomplete.setTypes(["establishment"]);
 
-document.addEventListener('DOMContentLoaded', function () {
-  autocomplete.addListener('place_changed', function () {
+document.addEventListener("DOMContentLoaded", function() {
+  autocomplete.addListener("place_changed", function() {
     displayOpenHours();
   });
 });
 
-input.addEventListener('input', function () {
-  outputOpenHours.innerHTML = '';
-  outputIsOpen.innerHTML = '';
-  document.body.style.height = '270px';
+input.addEventListener("input", function() {
+  outputOpenHours.innerHTML = "";
+  outputIsOpen.innerHTML = "";
+  document.body.style.height = "270px";
 });
 
 function displayOpenHours() {
@@ -24,13 +24,31 @@ function displayOpenHours() {
   if (!place.opening_hours) {
     outputOpenHours.innerHTML = "Sorry, no info available";
   } else {
-    outputIsOpen.innerHTML = place.opening_hours.open_now ? 'Yes' : 'No';
+    outputIsOpen.innerHTML = place.opening_hours.open_now ? "Yes" : "No";
   }
 
-  place.opening_hours.weekday_text.forEach(openHour => {
-    let p = document.createElement("P"); 
-    let text = document.createTextNode(openHour); 
-    p.appendChild(text); 
-    outputOpenHours.appendChild(p);
-  });
+  sortDays(place.opening_hours.weekday_text, new Date().getDay()).forEach(
+    openHour => {
+      let div = document.createElement("DIV");
+      div.style.display = "flex";
+      div.style.justifyContent = "space-between";
+
+      let dayElem = document.createElement("P");
+      let dayText = document.createTextNode(openHour.split(/ (.+)/)[0]);
+      let timeElem = document.createElement("P");
+      let timeText = document.createTextNode(openHour.split(/ (.+)/)[1]);
+      dayElem.appendChild(dayText);
+      timeElem.appendChild(timeText);
+      div.appendChild(dayElem);
+      div.appendChild(timeElem);
+      outputOpenHours.appendChild(div);
+    }
+  );
+}
+
+function sortDays(days, today) {
+  for (let i = 0; i < today; i++) {
+    days.push(days.shift());
+  }
+  return days;
 }
